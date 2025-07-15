4943f713ad63d5a74ef5393569f40d2094190b56
sofiso99 <sofrodasg@gmail.com>
2025-07-15 22:20:27 +0200

    made a second change to index and script files for demo

--- script.js
+++ script.js
@@ -3,3 +3,4 @@
 console.log("Welcome to GitHub practice!");
 console.log("I think Katy is keeping me honest and I'm here for it!!");
 console.log("This is an example");
+console.log("This is a second change");
